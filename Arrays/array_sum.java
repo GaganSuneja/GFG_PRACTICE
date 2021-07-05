108a53c1902a7237b8c22e229d0f8a462307790e
gagan suneja <gagan91suneja@gmail.com>
2021-07-06 00:30:59 +0530

    array_sum

--- Arrays/array_sum.java
+++ Arrays/array_sum.java
@@ -1,5 +1,28 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class GFGPractice{
     public static void main(String args[]) {
-        System.out.println("gagan");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(0);
+        System.out.println(longestSumof1s(list));
+        
     }   
+
+    public static int longestSumof1s(List<Integer> list) {
+        int maxSum = 0;
+        int auxSum = 0;
+        for(int i=0;i<list.size();i++) {
+            auxSum = list.get(i).intValue() == 1? auxSum+1 :  0;
+            maxSum = auxSum > maxSum ? auxSum:maxSum;
+        }
+        return maxSum;
+    }
 }
