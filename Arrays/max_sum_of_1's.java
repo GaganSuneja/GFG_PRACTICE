11353a911d873cec1dd2c6da588b9ac77f9a37e6
gagan suneja <gagan91suneja@gmail.com>
2021-07-06 14:58:21 +0530

    arrays

---
+++ Arrays/max_sum_of_1's.java
@@ -0,0 +1,23 @@
+package Arrays;
+// GFG LINK
+//  https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
+public class MaxSumOf1s {
+    
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxSum = 0;
+        int auxSum = 0;
+        for(int i =0;i<nums.length;i++) {
+            if(nums[i] == 1) {
+                auxSum++;
+            }else {
+                auxSum = 0;
+            }
+            if(auxSum > maxSum) {
+                maxSum = auxSum;
+            }
+        }
+        return maxSum;
+    }
+}
+}
