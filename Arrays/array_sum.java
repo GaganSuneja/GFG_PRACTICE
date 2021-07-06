77bb419497042ba2f4f2d9c8ac0a8667e034368f
gagan suneja <gagan91suneja@gmail.com>
2021-07-06 15:19:37 +0530

    arrays

--- Arrays/array_sum.java
+++ Arrays/array_sum.java
@@ -4,17 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 class GFGPractice{
+    
     public static void main(String args[]) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(0);
-        System.out.println(longestSumof1s(list));
+        // List<Integer> list = new ArrayList<>();
+        // list.add(1);
+        // list.add(1);
+        // list.add(1);
+        // list.add(1);
+        // list.add(1);
+        // list.add(0);
+        // System.out.println(longestSumof1s(list));
+        // Solution s  = new Solution();
+        int input[] = {12,345,2,6,7896};
+        findNumbers(input);
         
     }   
+
+    public static int findNumbers(int[] nums) {
+        int noOfEventDigits = 0;
+        String noInString = "";
+        for(int i =0;i<nums.length;i++) {
+            noInString = String.valueOf(nums[i]);
+            int k = (noInString.length() % 2 == 0)? noOfEventDigits++:0;
+        }
+        return noOfEventDigits;
+        
+    }
 
     public static int longestSumof1s(List<Integer> list) {
         int maxSum = 0;
@@ -24,5 +39,9 @@ class GFGPractice{
             maxSum = auxSum > maxSum ? auxSum:maxSum;
         }
         return maxSum;
+    }
+
+    class Solution {
+       
     }
 }
--- Arrays/max_sum_of_1's.java
+++ Arrays/max_sum_of_1's.java
@@ -19,5 +19,7 @@ class Solution {
         }
         return maxSum;
     }
+    String s = "abs";
+    
 }
 }
